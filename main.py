ab6607a95a8d1e3b5bbd6939400310c276f0e71f
Satyaseelan <satyaseelan@gmail.com>
2020-11-09 20:12:35 +1100

    Added main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator")
+name = input("What is your name?\n")
+city = input("Which city did you grew up in?\n")
+pet = input("What is your pet's name?\n")
+uppercase_name = city.upper() + " " + pet.upper()
+
+print("\nHi " + name + ", your band name is " + uppercase_name + ".")
